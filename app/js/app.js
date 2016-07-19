c38c414c5fc2a296b8414e0163ddd3cd0f0965f6
leoeatlesair <liuyitao811@hotmail.com>
2016-07-19 20:08:04 +0800

    I want to do something different

---
+++ app/js/app.js
@@ -0,0 +1,27 @@
+var data = [{
+  link: '##',
+  title: '设计',
+  subCols: 2,
+  subMenu: [{link: '##', title: '绘图'}, {link: '##', title: '手工'}, {link: '##', title: '家居'}]
+},
+  {link: '##', title: '玄学', subCols: 3, subMenu: [{link: '##', title: '占星'}, {link: '##', title: '血型'}]},
+  {link: '#c3', title: '视听', subCols: 4, subMenu: [{link: '##', title: '视频'}, {link: '##', title: '对话'}]},
+  {
+    link: '##',
+    title: '套路',
+    subCols: 3,
+    subMenu: [{link: '##', title: '约会着装设计'}, {link: '##', title: '面试着装设计'}, {link: '##', title: '家居着装设计'}]
+  },
+  {
+    link: '##',
+    title: '上门',
+    subCols: 3,
+    subMenu: [{link: '##', title: '代办'}, {link: '##', title: '家教'}, {link: '##', title: '化妆'}]
+  },
+  {link: '##', title: '摄影', subCols: 3, subMenu: [{link: '##', title: '拍图'}, {link: '##', title: '修图'}]},
+  {
+    link: '##',
+    title: '咨询',
+    subCols: 3,
+    subMenu: [{link: '##', title: '职业规划'}, {link: '##', title: '恋爱'}, {link: '##', title: '留学规划'}]
+  }];
