660bee29c949f19e858913645c70ee1ca29e5dd8
LeoEatle <liuyitao811@hotmail.com>
2016-08-09 09:59:53 +0800

    V1.5This is a big commit! There is a very important part which confused me for a long time That is the error Uncaught TypeError: Cannot read property '__reactInternalInstance$td5jlimx8c5yb24vl9v6os9k9' of null This is because when I import the component defined by myself, I use import {ProductInstance} from '../component/ProductInstance' But it's wrong. When the component use 'export default ProductInstance', we dont need to add {}. Just use import ProductInstance from '../component/ProductInstance' --------------------------------------- The update includes the ServiceDetail.js and some pictures And some adust for models And the routers -------------------------------------- Fuck React.js. I want to develop iOS! or vue.js !

--- app/js/app.js
+++ app/js/app.js
@@ -1,27 +1,90 @@
-var data = [{
-  link: '##',
-  title: '设计',
-  subCols: 2,
-  subMenu: [{link: '##', title: '绘图'}, {link: '##', title: '手工'}, {link: '##', title: '家居'}]
-},
-  {link: '##', title: '玄学', subCols: 3, subMenu: [{link: '##', title: '占星'}, {link: '##', title: '血型'}]},
-  {link: '#c3', title: '视听', subCols: 4, subMenu: [{link: '##', title: '视频'}, {link: '##', title: '对话'}]},
-  {
-    link: '##',
-    title: '套路',
-    subCols: 3,
-    subMenu: [{link: '##', title: '约会着装设计'}, {link: '##', title: '面试着装设计'}, {link: '##', title: '家居着装设计'}]
+import React, { Component } from 'react';
+import {
+  render
+} from 'react-dom';
+import {
+  Router,
+  Route,
+  IndexRoute,
+  hashHistory,
+} from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {deepOrange500} from 'material-ui/styles/colors';
+import SiteFooter from './components/SiteFooter';
+import MenuInstance from './components/MenuInstance';
+import Navbar_bs from './components/Navbar_bs'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
   },
-  {
-    link: '##',
-    title: '上门',
-    subCols: 3,
-    subMenu: [{link: '##', title: '代办'}, {link: '##', title: '家教'}, {link: '##', title: '化妆'}]
+});
+
+const App = React.createClass({
+  getInitialState: function () {
+    return{
+      logged: this.checklogged(),
+    }
   },
-  {link: '##', title: '摄影', subCols: 3, subMenu: [{link: '##', title: '拍图'}, {link: '##', title: '修图'}]},
-  {
-    link: '##',
-    title: '咨询',
-    subCols: 3,
-    subMenu: [{link: '##', title: '职业规划'}, {link: '##', title: '恋爱'}, {link: '##', title: '留学规划'}]
-  }];
+
+  checklogged: function () {
+    let username = localStorage.getItem('username');
+    if(username != null){
+      console.log('now the user is ' + username);
+    }
+    return username || null;
+  },
+
+  onlogin: function (username) {
+    localStorage.setItem('username',username);
+    this.setState({logged: username });
+  },
+
+  render: function () {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="ask-page">
+          <Navbar_bs username={this.state.logged}></Navbar_bs>
+          <MenuInstance />
+          <main className="ask-main">
+            {this.props.children}
+          </main>
+          <SiteFooter />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+});
+
+// Pages
+import Index from './pages/Index';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
+import Category from './pages/Category';
+//import userRegister from './pages/userRegister';
+import sellerRegister from './pages/sellerRegister';
+import ServiceDetail from './pages/ServiceDetail';
+
+const routes = (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Index} />
+      <Route path="/page1" component={Page1} />
+      <Route path="/page2" component={Page2} />
+      {/*<Route path="/userRegister" component={userRegister} />*/}
+      <Route path="/sellerRegister" component={sellerRegister} />
+      <Route path="/Categories/:category" component={Category} />
+      <Route path="/details/:userid/:serviceid" component={ServiceDetail} />
+    </Route>
+  </Router>
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(routes, document.getElementById('root'));
+});
